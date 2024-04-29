e3bfafbff0124e2d1325fff7ea8e68ba30e13a30
jiovani fabro <you@example.com>
2024-04-29 16:42:55 +0800

    cords3

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ App.use(Cors());
 App.use(Express.json())
 
 
-
+App.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 
 //connection
